8c206767e3b547dd4aa1af26ec7256ce384230b7
dylanshangold <dylan.shangold@gmail.com>
2024-07-22 21:29:49 -0400

    refactor

--- scripts/index.js
+++ scripts/index.js
@@ -27,12 +27,32 @@ const initialCards = [
 
 const profileEditBtn = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileCloseBtn = document.querySelector("#profile-close-button");
+const profileTitle = document.querySelector("#profile-title");
+const profileDescription = document.querySelector("#profile-description");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector(
+  "#profile-description-input"
+);
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+
+function closePopup() {
+  profileEditModal.classList.remove("modal_opened");
+}
+
+function handleProfileEditSubmit(e) {
+  e.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value.trim();
+  closePopup();
+}
 
 profileEditBtn.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent.trim();
   profileEditModal.classList.add("modal_opened");
 });
 
-const profileCloseBtn = document.querySelector("#profile-close-button");
-profileCloseBtn.addEventListener("click", () => {
-  profileEditModal.classList.remove("modal_opened");
-});
+profileCloseBtn.addEventListener("click", closePopup);
+
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
